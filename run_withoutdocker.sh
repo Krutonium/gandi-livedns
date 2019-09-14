e988eb089b58431a0fb59b1a11300273f7569df5
Krutonium <PFCKrutonium@gmail.com>
2019-09-14 05:12:15 -0400

    Add ability to run outside docker

---
+++ run_withoutdocker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export APIKEY=
+export RECORDLIST=
+export DOMAIN=
+
+#APIKEY == Your API Key
+#Record List == @ for root domain, www,mail,whatever for subdomains, semicolons.
+#Domain == Your domain - resindrake.net
+
+echo Variables Set
+
+./run.sh
