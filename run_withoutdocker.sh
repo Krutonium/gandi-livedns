bda0f3ca3953c6b3c60756daa516029b7fb41ad3
Krutonium <PFCKrutonium@gmail.com>
2019-09-14 05:14:22 -0400

    Add more vars

--- run_withoutdocker.sh
+++ run_withoutdocker.sh
@@ -3,9 +3,14 @@ export APIKEY=
 export RECORDLIST=
 export DOMAIN=
 
+export SET_IPV4=no
+export SET_IPV6=no
+
 #APIKEY == Your API Key
 #Record List == @ for root domain, www,mail,whatever for subdomains, semicolons.
 #Domain == Your domain - resindrake.net
+
+#change the SET to yes if you want that one updated.
 
 echo Variables Set
 
